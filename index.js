7525989636cef88a7f9ffdab4cb1741a9540f625
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-01 22:56:36 +0600

    added encrypted, decrypted text based on the Vigenere cipher;

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const Vigenere = require('caesar-salad').Vigenere;
+const password = 'password';
+
 app.get('/', (req, res) => {
     return res.send('<h2>This is a home page</h2>');
 });
 
 app.get('/:echo', (req, res) => {
-    return res.send(`<h1>${req.params.echo}!</h1>`);
+    return res.send(`<h1>${req.params.echo}</h1>`);
+});
+
+app.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encodedText = Vigenere.Cipher(password).crypt(text);
+    return res.send(encodedText);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decodedText = Vigenere.Decipher(password).crypt(text);
+    return res.send(decodedText);
 });
 
 app.listen(port, () => {
